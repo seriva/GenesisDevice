cc696e4df464c5b2d1c37d5ea724516a9e62201c
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2019-05-15 17:58:55 +0200

    Added binary files for debian and install script for dependencies.

--- Bin/debian-dependancies.sh
+++ Bin/debian-dependancies.sh
@@ -2,8 +2,8 @@ apt-get install libsdl2-dev libopenal-dev libmpg123-dev
 apt-get install build-essential cmake libtinyxml-dev libglfw3-dev
 wget https://github.com/MADEAPPS/newton-dynamics/archive/newton-3.14.zip; unzip newton-3.14.zip; rm newton-3.14.zip
 cd newton-dynamics-newton-3.14
-mkdir -r build && cd build
+mkdir build && cd build
 cmake -DNEWTON_DEMOS_SANDBOX=OFF .. && make
-yes | cp -rf ./lib/libNewton.so /usr/bin/
+sudo cp -rf ./lib/libNewton.so /usr/bin/
 cd ../../
 rm -rf newton-dynamics-newton-3.14
